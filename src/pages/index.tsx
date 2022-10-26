df0b87f27c5dca87a29457f90ed8ff9ea1aa49d4
Jcanotorr06 <jcano062001@gmail.com>
2022-10-26 17:15:54 -0500

    update: pages/index.tsx add buttons to preview styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,32 @@ const LandingPage:NextPage = () => {
     return(
         <>
             <h1>Hello World</h1>
+            <div className="flex flex-wrap gap-2">
+                <button className="btn btn-lg btn-primary">Button</button>
+                <button className="btn btn-lg btn-secondary">Button</button>
+                <button className="btn btn-lg btn-neutral">Button</button>
+                <button className="btn btn-lg btn-success">Button</button>
+                <button className="btn btn-lg btn-warning">Button</button>
+                <button className="btn btn-lg btn-error">Button</button>
+            </div>
+            <div className="flex flex-wrap gap-2">
+                <button className="btn btn-md btn-primary">Button</button>
+                <button className="btn btn-md btn-secondary">Button</button>
+                <button className="btn btn-md btn-neutral">Button</button>
+                <button className="btn btn-md btn-success">Button</button>
+                <button className="btn btn-md btn-warning">Button</button>
+                <button className="btn btn-md btn-error">Button</button>
+            </div>
+            <div className="flex flex-wrap gap-2">
+                <button className="btn btn-sm btn-primary">Button</button>
+                <button className="btn btn-sm btn-secondary">Button</button>
+                <button className="btn btn-sm btn-neutral">Button</button>
+                <button className="btn btn-sm btn-success">Button</button>
+                <button className="btn btn-sm btn-warning">Button</button>
+                <button className="btn btn-sm btn-error">Button</button>
+            </div>
         </>
     )
 }
+
+export default LandingPage
