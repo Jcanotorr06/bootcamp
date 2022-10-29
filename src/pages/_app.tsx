4432b50e4c079c3a32f37ee75edbe19499ccee78
Jcanotorr06 <jcano062001@gmail.com>
2022-10-28 23:30:38 -0500

    update: _app.tsx. add DefaultSeo component to root of app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { SessionProvider } from "next-auth/react";
 import type { Session } from "next-auth";
 import type { AppType } from "next/app";
 import { trpc } from "../utils/trpc";
+import { DefaultSeo } from "next-seo";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -11,6 +12,12 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <DefaultSeo
+        title="Bootcamp Panamá"
+        description="Inicie e impulse su carrera tecnológica rápidamente. ¿Listo para aceptar un trabajo mejor pagado? Sigue leyendo para aprender.
+Somos Bootcamp Panamá. Nuestra misión es introducirlo en la industria de TI."
+        themeColor="#FFFFFF"
+      />
       <Component {...pageProps} />
     </SessionProvider>
   );
